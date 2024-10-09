141d4833f3069d856efd7d28be8e99252797ed80
Naman Gupta <namangupta2360@gmail.com>
2024-10-09 14:34:07 +0530

    dc9

--- model.py
+++ model.py
@@ -71,4 +71,4 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='44.226.145.213', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
